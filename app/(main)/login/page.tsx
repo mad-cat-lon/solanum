b84f4e5bc8a80078f3b95f2e552015ac8fb18681
mad-cat-lon <113548315+mad-cat-lon@users.noreply.github.com>
2024-09-07 12:31:57 -0400

    Tweak mobile layout

--- app/(main)/login/page.tsx
+++ app/(main)/login/page.tsx
@@ -4,10 +4,9 @@ import { OrSeparator } from "@/components/ui/or-separator";
 
 export default function LoginPage() {
   return (
-    <div className="grow flex flex-col items-center justify-center">
-      <section className="w-[32rem] space-y-4">
-        <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl mb-6">
-          Login Example
+    <div className="grow flex flex-col items-center justify-center px-4 sm:px-6">
+      <section className="w-full max-w-md space-y-4">
+        <h1 className="scroll-m-20 text-3xl font-extrabold tracking-tight sm:text-4xl lg:text-5xl mb-6 text-center">
         </h1>
         <AuthCard />
         <OrSeparator />
