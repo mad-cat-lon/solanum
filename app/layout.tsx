6bd73fd87447837a86b19a23de5bd773dfe01ff9
mad-cat-lon <113548315+mad-cat-lon@users.noreply.github.com>
2024-09-30 21:56:52 -0400

    Suppress hydration warning for next-themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${workSans.className} ${orbitron.variable} ${pressStart2P.variable}`}>
         <ThemeProvider
           attribute="data-theme"
